ae98202298c7f4e33c2562dba3ae123d82eaafd1
H1ggsK <theohavel@proton.me>
2024-12-10 10:53:58 -0800

    Add files via upload

---
+++ src/main/java/com/h1ggsk/paperdupe/Paperdupe.java
@@ -0,0 +1,10 @@
+package com.h1ggsk.paperdupe;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Paperdupe implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
